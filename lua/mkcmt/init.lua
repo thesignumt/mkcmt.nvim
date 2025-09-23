f943493c343737372f016a4c8369abb61762631b
thesignumt <m96153045@gmail.com>
2025-09-23 16:59:40 +0100

    feat: using OOP

--- lua/mkcmt/init.lua
+++ lua/mkcmt/init.lua
@@ -1,98 +1,35 @@
-local M = {}
+local Config = require("mkcmt.config")
+local Utils = require("mkcmt.utils")
 
--- +-------------------------------------------------------+
--- [                        config                         ]
--- +-------------------------------------------------------+
-local config = {
-  default_header = "HELLO WORLD",
-  cmd = true,
-  min_width = 60, -- minimum width of the block
-  padding = 10, -- extra spacing around header
-  border = "+-+[]+-+",
-}
+---@class Mkcmt
+---@field config MkcmtConfig
+local Mkcmt = {}
 
--- +-------------------------------------------------------+
--- [                         setup                         ]
--- +-------------------------------------------------------+
---- @class mkcmt.setup.Opts
---- @inlinedoc
----
---- The default header when no header is provided when there is a prompt.
---- @field default_header? string
----
---- If true will make a user command MkCmt
---- @field cmd? boolean
----
---- Minimum width of the block
---- @field min_width? integer
----
---- Extra spacing around header
---- @field padding? integer
----
---- Borders..
---- @field border? string
+Mkcmt.__index = {}
 
---- Setup MkCmt user preferences
---- @param opts? mkcmt.setup.Opts
---- @return nil
-function M.setup(opts)
-  config = vim.tbl_extend("force", config, opts or {})
-
-  if config.cmd then
-    vim.api.nvim_create_user_command("MkCmt", M.comment, {})
-  end
-end
-
--- +-------------------------------------------------------+
--- [                       functions                       ]
--- +-------------------------------------------------------+
-
----delete last selection
----@param visual boolean
----@return nil
-local function del_lsel(visual)
-  if not visual then
-    return
-  end
-
-  vim.api.nvim_buf_set_lines(
-    0,
-    vim.fn.line("'<") - 1,
-    vim.fn.line("'>"),
-    false,
-    {}
-  )
-end
-
-local function get_comment_str()
-  local cs = vim.bo.commentstring or "# %s"
-  local pre, suf = cs:match("^(.*)%%s(.*)$")
-  return pre or "", suf or ""
-end
-
-local function get_borders(str)
-  return {
-    { str:sub(1, 1), str:sub(2, 2), str:sub(3, 3) },
-    { str:sub(4, 4), str:sub(5, 5) },
-    { str:sub(6, 6), str:sub(7, 7), str:sub(8, 8) },
-  }
+---@return Mkcmt
+function Mkcmt:new()
+  local mkcmt = setmetatable({
+    config = Config,
+  }, self)
+  return mkcmt
 end
 
 ---make comment block
 ---@param header string
 ---@param opts mkcmt.comment.Opts
 ---@param data table
----@return nil
-local function mkcmt(header, opts, data)
+---@return Mkcmt
+function Mkcmt:_mkcmt(header, opts, data)
   local function get(opt, fallback)
-    return vim.F.if_nil(opts[opt], config[opt]) or fallback
+    return vim.F.if_nil(opts[opt], self.config[opt]) or fallback
   end
-  local pre, suf = get_comment_str()
+  local pre, suf = Utils:get_comment_str()
   local min_width = get("min_width")
   ---@cast min_width integer
   local total_width = math.max(min_width, #header + get("padding") * 2)
 
-  local b = get_borders(get("border", "+-+[]+-+"))
+  local b = Utils:get_borders(get("border", "+-+[]+-+"))
 
   -- Center the header
   local space = total_width - #header - #pre - #suf
@@ -117,8 +54,10 @@ local function mkcmt(header, opts, data)
   local dl = mkline(b[3])
 
   local lines = { ul, mdl, dl }
-  del_lsel(data.visual)
+  Utils:del_lsel(data.visual)
   vim.api.nvim_put(lines, "l", data.after, true)
+
+  return self
 end
 
 -- +-------------------------------------------------------+
@@ -132,8 +71,8 @@ end
 
 ---make a comment block
 ---@param opts? mkcmt.comment.Opts
----@return nil
-function M.comment(opts)
+---@return Mkcmt
+function Mkcmt:comment(opts)
   opts = opts or {}
   local visual = vim.fn.mode() == "V"
   local after = opts.after == true
@@ -143,14 +82,35 @@ function M.comment(opts)
   local prompt = (upper and "(upper) " or "") .. "header: "
 
   if opts.header then
-    mkcmt(opts.header, opts, data)
+    self:_mkcmt(opts.header, opts, data)
   else
     vim.ui.input({ prompt = prompt }, function(input)
+      local header = input == "" and self.config.default_header or input
       if input then
-        mkcmt(input == "" and config.default_header or input, opts, data)
+        self:_mkcmt(header, opts, data)
       end
     end)
   end
+
+  return self
 end
 
-return M
+local the_mkcmt = Mkcmt:new()
+
+--- Setup MkCmt
+--- @param opts? MkcmtConfig
+--- @return Mkcmt
+function Mkcmt:setup(opts)
+  self.config = vim.tbl_extend("force", self.config, opts or {})
+
+  if self.config.cmd then
+    -- TODO: extend this. maybe :Mkcmt upper... etc
+    vim.api.nvim_create_user_command("MkCmt", function()
+      self:comment()
+    end, {})
+  end
+
+  return self
+end
+
+return the_mkcmt
